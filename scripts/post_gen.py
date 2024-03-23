ef6c810f0632a18228e6bee7240a9cddb7eb9982
air391 <wangqd@foxmail.com>
2024-03-23 14:38:03 +0800

    feat: generate posts

---
+++ scripts/post_gen.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+# 输入和输出目录
+input_dir = 'posts'
+output_dir = 'contents'
+
+# 确保输出目录存在
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# 遍历输入目录中的所有文件
+for filename in os.listdir(input_dir):
+    if filename.endswith('.md'):
+        # 构造输入和输出文件的路径
+        input_file = os.path.join(input_dir, filename)
+        output_file = os.path.join(output_dir, os.path.splitext(filename)[0] + '.html')
+        
+        # 使用 Pandoc 将 Markdown 文件转换为 HTML
+        subprocess.run(['pandoc', input_file, '-o', output_file])
+
+print("Markdown 文件转换完成。")
